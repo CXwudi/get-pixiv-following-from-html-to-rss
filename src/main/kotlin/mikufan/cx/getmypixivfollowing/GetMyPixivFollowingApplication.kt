f5f17584dc3dcf628459e6abb58b633a54770839
CXwudi <cx2010017@gmail.com>
2021-06-16 12:50:05 -0400

    😮 spring native support

--- src/main/kotlin/mikufan/cx/getmypixivfollowing/GetMyPixivFollowingApplication.kt
+++ src/main/kotlin/mikufan/cx/getmypixivfollowing/GetMyPixivFollowingApplication.kt
@@ -3,7 +3,10 @@ package mikufan.cx.getmypixivfollowing
 import mikufan.cx.getmypixivfollowing.service.MainService
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.nativex.hint.AccessBits
+import org.springframework.nativex.hint.TypeHint
 
+@TypeHint(types = [kotlin.text.Regex::class], access = AccessBits.FULL_REFLECTION)
 @SpringBootApplication
 class GetMyPixivFollowingApplication
 
